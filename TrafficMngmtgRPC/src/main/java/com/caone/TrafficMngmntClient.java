935cff24630cbc6434e1261f529fbee41128ca9c
chandlerbiing <winnie.mensah-ogun@bath.edu>
2021-08-06 10:12:39 +0200

    First commit

---
+++ TrafficMngmtgRPC/src/main/java/com/caone/TrafficMngmntClient.java
@@ -0,0 +1,37 @@
+package com.caone;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+
+
+
+public class TrafficMngmntClient {
+	
+	public static void main (String[] args) throws Exception {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
